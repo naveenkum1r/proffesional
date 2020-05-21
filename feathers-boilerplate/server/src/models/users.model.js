d27e0d3cb64c1ba312df1c482f0537cb2053c1b3
naveenkum1r <naveenkumar199420@yahoo.in>
2020-05-21 16:44:28 +0530

    :bug: Bug with vuetify not able to install over airbnb
    reinstalled vuetify over vue

--- feathers-boilerplate/server/src/models/users.model.js
+++ feathers-boilerplate/server/src/models/users.model.js
@@ -7,9 +7,23 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
   
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-  
+    username: { 
+      type: String, 
+      unique: true, 
+      lowercase: true 
+    },
+    displayName:{
+      type:String,
+      required:true
+    },
+    imageUrl:{
+      type:String,
+      required:true
+    },
+    password: { 
+      type: String,
+      required:true 
+    },
   
   }, {
     timestamps: true
